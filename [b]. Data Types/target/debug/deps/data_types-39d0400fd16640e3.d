644587221fb0613969cc4bea62d17bf6038187db
vinaykanna <vinaykanna367@gmail.com>
2024-04-26 12:01:45 +0530

    practise rust

--- [b]. Data Types/target/debug/deps/data_types-39d0400fd16640e3.d
+++ [b]. Data Types/target/debug/deps/data_types-39d0400fd16640e3.d
@@ -1,5 +1,5 @@
-/Users/vinay.kumar/Desktop/Learnings/Rust-Fundaments/data_types/target/debug/deps/data_types-39d0400fd16640e3.rmeta: src/main.rs
+/Users/vinay.kumar/Desktop/Learnings/Rust-Fundaments/[b]. Data Types/target/debug/deps/data_types-39d0400fd16640e3.rmeta: src/main.rs
 
-/Users/vinay.kumar/Desktop/Learnings/Rust-Fundaments/data_types/target/debug/deps/data_types-39d0400fd16640e3.d: src/main.rs
+/Users/vinay.kumar/Desktop/Learnings/Rust-Fundaments/[b]. Data Types/target/debug/deps/data_types-39d0400fd16640e3.d: src/main.rs
 
 src/main.rs:
--- [b]. Data Types/target/debug/deps/data_types-a298945b21415019.d
+++ [b]. Data Types/target/debug/deps/data_types-a298945b21415019.d
@@ -1,5 +1,5 @@
-/Users/vinay.kumar/Desktop/Learnings/Rust-Fundaments/data_types/target/debug/deps/data_types-a298945b21415019.rmeta: src/main.rs
+/Users/vinay.kumar/Desktop/Learnings/Rust-Fundaments/[b]. Data Types/target/debug/deps/data_types-a298945b21415019.rmeta: src/main.rs
 
-/Users/vinay.kumar/Desktop/Learnings/Rust-Fundaments/data_types/target/debug/deps/data_types-a298945b21415019.d: src/main.rs
+/Users/vinay.kumar/Desktop/Learnings/Rust-Fundaments/[b]. Data Types/target/debug/deps/data_types-a298945b21415019.d: src/main.rs
 
 src/main.rs:
